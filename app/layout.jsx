423b4e37fc15af791d3cbd6657cb28e1841d976b
FinoanaRandria <fitiavanarex4@gmail.com>
2023-11-19 14:04:58 +0300

    initialize layout

---
+++ app/layout.jsx
@@ -0,0 +1,26 @@
+import '@styles/globals.css'
+
+
+export const metadata = {
+ title:"Promptopia",
+ descritption:"Discover & Share Ai promps"
+
+}
+
+const layout = () => {
+  return (
+    <div>
+        <html lang="en">
+            <body>
+                <div classname="main">
+                    <div classname="gradient">
+
+                    </div>
+                </div>
+            </body>
+        </html>
+    </div>
+  )
+}
+
+export default layout

1cfda974835bf23a876f354693dfa385ab3b1c4e
Finoana Randri <110087713+FinoanaRandria@users.noreply.github.com>
2023-11-20 10:56:30 +0100

    provider

--- components/Provider.jsx
+++ components/Provider.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import { SessionProvider } from 'next-auth/react'
 
-const Provider = () => {
+
+const Provider = ({childre,session}) => {
   return (
     <div>Provider</div>
   )

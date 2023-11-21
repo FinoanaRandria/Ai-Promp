f5fc9ccac41121a584f0bdef1ed06aa327584a90
Finoana Randri <110087713+FinoanaRandria@users.noreply.github.com>
2023-11-21 08:03:45 +0100

    providers

--- app/layout.jsx
+++ app/layout.jsx
@@ -9,15 +9,20 @@ export const metadata = {
 
 const RootLayout = ({ children }) => {
   return (
-    <html lang="en">
+    <html lang="en">   
+
         <body>
-            <div className="main">
+             <Provider>
+
+              <div className="main">
                 <div className="gradient" />
             </div>
                     <main className="app">
                         <Nav />
                             {children}
                    </main>
+             </Provider>
+            
         </body>
     </html>
   );

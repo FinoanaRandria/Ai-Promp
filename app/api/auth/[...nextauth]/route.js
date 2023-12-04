204a62583c90ae7efbf3e24c19b5a5d952ba2153
Finoana Randri <110087713+FinoanaRandria@users.noreply.github.com>
2023-12-04 23:51:04 +0100

    Update route.js

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -6,7 +6,7 @@ import GoogleProvider from "next-auth/providers/google";
 
 import { connectToDB } from "@utils/database";
 
-
+import User from "@models/user";
 //console.log({
  // clientId:process.env.GOOGLE_ID,
   //clientSecret:process.env.GOOGLE_CLIENT_SECRET,
@@ -27,10 +27,22 @@ const handler = NextAuth({
   async signIn({profile}){
       try {
         await connectToDB()
+       
+        const userExist = await User.findOne({
+          email:profile.email
+        })
 
-
+        if(!userExist){
+          await User.create({
+            email:profile.email,
+            username:profile.name.replace(" ","").toLowerCase(),
+            image:profile.picture
+          })
+        }
         return true
       } catch (error) {
+
+        
          console.log(error);
          return false
       }

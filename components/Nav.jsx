65a1366dadaa94b0a9967da8bc9865c962b34d92
Finoana Randri <110087713+FinoanaRandria@users.noreply.github.com>
2023-12-05 22:02:42 +0100

    update nav and user

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -4,7 +4,7 @@ import Link from "next/link"
 import Image from "next/image"
 import {useState , useEffect} from 'react'
 import {signIn, signOut, useSession , getProviders} from 'next-auth/react'
-import User from "@models/user"
+
 
 
 const Nav = () => {
@@ -41,8 +41,9 @@ const [toggleDropdown,setToggleDropdown]= useState(false)
          
 
     </Link>
-
-      {/* Desktop  Navigation */}
+       
+      {/*  Desktop  Navigation */}
+         
            <div className="sm:flex hidden">
                  {session?.user ? (
                     <div className="flex gap-3 md:gap-5">
@@ -89,7 +90,7 @@ const [toggleDropdown,setToggleDropdown]= useState(false)
                 session?.user ? (
                    <div className="flex" >
                       <Image
-                          src="/assets/images/logo.svg"
+                          src={session?.user.image}
                           width={37}
                           height={37}
                           className="rounded-full"

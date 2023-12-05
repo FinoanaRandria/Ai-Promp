ef2ad49fd297ce0a74df2381536f179b5094ab37
Finoana Randri <110087713+FinoanaRandria@users.noreply.github.com>
2023-12-05 20:10:43 +0100

    modif model

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -4,11 +4,12 @@ import Link from "next/link"
 import Image from "next/image"
 import {useState , useEffect} from 'react'
 import {signIn, signOut, useSession , getProviders} from 'next-auth/react'
+import User from "@models/user"
 
 
 const Nav = () => {
 
-  const isUserLoggedIn = true;
+ const {data:session}= useSession()
 
 const [providers,setProviders]= useState(null)
 const [toggleDropdown,setToggleDropdown]= useState(false)
@@ -16,12 +17,12 @@ const [toggleDropdown,setToggleDropdown]= useState(false)
 
 
  useEffect(()=>{
-   const setProviders = async ()=>{
+   const setUpProviders = async ()=>{
       const response = await getProviders()
 
       setProviders(response)
    }
-    setProviders()
+    setUpProviders()
  },[])
 
   return (
@@ -43,7 +44,7 @@ const [toggleDropdown,setToggleDropdown]= useState(false)
 
       {/* Desktop  Navigation */}
            <div className="sm:flex hidden">
-                 {isUserLoggedIn ? (
+                 {session?.user ? (
                     <div className="flex gap-3 md:gap-5">
                             <Link className="black_btn" href="/create-promp">
                                Create Post
@@ -85,7 +86,7 @@ const [toggleDropdown,setToggleDropdown]= useState(false)
          {/* Mobile navigation */}
             <div className="sm:hidden flex relative ">
               {
-                isUserLoggedIn ? (
+                session?.user ? (
                    <div className="flex" >
                       <Image
                           src="/assets/images/logo.svg"

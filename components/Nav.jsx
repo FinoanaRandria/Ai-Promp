15851e91c70fc9e2764b374abbecd33577764438
FinoanaRandria <110087713+FinoanaRandria@users.noreply.github.com>
2023-12-08 09:31:56 +0300

    add CallBack, modif env Mongo URI  Modif Image Nav

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -58,7 +58,7 @@ const [toggleDropdown,setToggleDropdown]= useState(false)
                        <Link href="/profile">
                                      
                           <Image
-                            src="/assets/images/logo.svg"
+                            src={session?.user.image}
                             width={37}
                             height={37}
                             className="rounded-full"
--- utils/database.js
+++ utils/database.js
@@ -13,8 +13,8 @@ export const connectToDB = async()=>{
     try {
         await mongoose.connect(process.env.MONGODB_URI,{
             dbName:'share_prompt',
-            useNewUrlParser:true,
-            useUnifedTopology:true,
+            useNewUrlParser : true,
+            useUnifiedTopology : true
         })
         isConnected = true
         console.log('MongoDb connected');

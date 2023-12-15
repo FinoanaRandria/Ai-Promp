63d90ab347a55b5a8445f66b2e4a7bb3627dc308
Finoana Randri <fitiavanarex4@gmail.com>
2023-12-15 23:06:02 +0300

    form seach

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,8 +1,36 @@
 
+'use client'
+
+import { useState, useEffect } from "react";
+import PromptCard from "./PromptCard";
 
 const Feed = () => {
+
+   const [searchText,setSearchText]=useState('')
+
+  const handleSearchChange = (e)=>{
+    
+      setSearchText(e.target.value)
+      console.log(searchText);
+      
+  }
+
   return (
-    <div>Feed</div>
+    <section className="feed">
+      <form className="relative w-full flex-center">
+
+       <input type="text"
+        placeholder="Search for text using tag"
+        value={searchText}
+        required
+        onChange={handleSearchChange}
+        className="search_input peer"
+       />
+
+      </form>
+       
+      
+    </section>
   )
 }
 

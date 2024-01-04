4c0259f3c2959d042bc49a7eb140db4fedd6da00
Finoana Randri <110087713+FinoanaRandria@users.noreply.github.com>
2024-01-04 23:55:12 +0300

    create new api

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -5,8 +5,15 @@ import  Prompt  from "@models/prompt"
  export const GET = async(response)=>{
   try {
     await connectToDB()
+
+    const prompts = await Prompt.find({}).populate('creator')
+
+    return new response(JSON.stringify(prompts), {
+        status:200
+    })
   } catch (error) {
-    
-  }
+    return new response("failed to fetch all prompts", {
+        status:500
+  })
 
 }

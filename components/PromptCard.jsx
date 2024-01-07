e26f4ac247eeb3233ce878c5b869eead6d6da931
Finoana Randri <110087713+FinoanaRandria@users.noreply.github.com>
2024-01-07 11:42:52 +0300

    handleCopy  function clipboard

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -8,6 +8,15 @@ import { usePathname,useRouter } from "next/navigation"
 const PromptCard = ({post,handleTagClick,handleEdit,handleDelete}) => {
    
     const [copied,setCopied]= useState("")
+
+    const handleCopy = ()=>{
+    setCopied(post.prompt)
+    window.Clipboard.writeText(post.prompt)
+    setTimeout(()=>{
+      setCopied("")
+    },3000)
+
+    }
   return (
     <div className="prompt_card">
          <div className="flex justify-between  items-start gap-5">
